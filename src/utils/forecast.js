0bf1073faa89e94cef79fc30cf26c3c975a15f25
mayank <mayank.j@servify.tech>
2020-05-20 17:51:33 +0530

    chamnging temp unit from f to c

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const request = require('request')
 
 const forecast = (lat, long, callback) => {
 
-  const url = 'http://api.weatherstack.com/current?access_key=f270e5771fd92aef30ef69106d24b98f&query='+ lat +',' + long +'&units=f'
+  const url = 'http://api.weatherstack.com/current?access_key=f270e5771fd92aef30ef69106d24b98f&query='+ lat +',' + long
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
